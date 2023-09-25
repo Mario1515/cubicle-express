a59022f164549076af731a2302a9fc52c7fd7581
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-09-25 12:53:17 +0300

    CUB-P1
    CUB-P1 intiliaze express server with folder structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from index.js");
+const express = require("express");
+const app = express();
+const PORT = 5050;
+
+app.get("/", (req, res) => {
+    res.send("Hello from express server!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
