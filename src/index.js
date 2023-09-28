907fbd0d2670fa3b9cf95913fa3237d0b3313296
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-09-28 15:30:26 +0300

    CUB-P1
    CUB-P1, modular routing, home and cube controllers intialized, fixed href in create

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ const express = require("express");
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
 const { PORT } = require("./constants.js");
+const routes = require("./router");
 
 // local variables
 const app = express();
-
 
 //configs
 handlebarsConfig(app);
 expressConfig(app);
 
-
 // routing
-app.get("/", (req, res) => {
-    res.render("index");
-});
+app.use(routes);
+
 // port listing
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
